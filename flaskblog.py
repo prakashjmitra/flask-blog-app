c92d050e3023745edce5902ed6354a9aea85a7c4
prakashjmitra <mitraprakash06@gmail.com>
2025-12-16 21:49:00 -0500

    Corey Flask Tutorial vids 1-2 done

--- flaskblog.py
+++ flaskblog.py
@@ -1,13 +1,27 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 app = Flask(__name__)
+posts = [
+    {
+        "author" : "Prakash Mitra",
+        "title" : "Blog Post 1",
+        "content" : "First Post Content",
+        "date_posted" : "December 16, 2025"
+    },
+    {
+        "author" : "Jane Doe",
+        "title" : "Blog Post 2",
+        "content" : "Second Post Content",
+        "date_posted" : "December 17, 2025"
+    }
+]
 @app.route("/")
 @app.route("/home")
 def home():
-    return render_template("home.html")
+    return render_template("home.html", posts = posts)
 
 @app.route("/about")
 def about():
-    return render_template("about.html")
+    return render_template("about.html", title = "About")
 
 if __name__ == "__main__":
     app.run(debug = True)
